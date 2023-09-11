2ac2bcf4aeb89b658b577d67786aef6df3421b45
David_ca6 <david@ca6.dev>
2023-09-11 17:12:37 -0400

    Update vscode.sh
    remplaced dnf vscode by flatpak vscode
    added copilot chat extension
    added vslabx extension for mplabx support
    added markdown preview extension

--- Workstation/vscode.sh
+++ Workstation/vscode.sh
@@ -1,21 +1,27 @@
 #!/bin/bash
 
+sudo dnf remove -y code
+flatpak install-y  flathub com.visualstudio.code
+
 # install code extension
-code --install-extension atlassian.atlascode                                    # jira issue
-code --install-extension GitHub.vscode-pull-request-github                      # github issue
-code --install-extension GitHub.remotehub                                       # github explorer
-code --install-extension GitHub.copilot                                         # github copilot ai
-code --install-extension Gruntfuggly.todo-tree                                  # todo from code
-code --install-extension ms-azuretools.vscode-docker                            # container
-code --install-extension ms-vscode-remote.remote-containers                     # remote contaienr
-code --install-extension ms-vscode-remote.remote-ssh                            # remote ssh
-code --install-extension ms-vscode-remote.remote-ssh-edit                       # edit remote ssh
-code --install-extension ms-vscode.cmake-tools                                  # smake tools support
-code --install-extension ms-vscode.cpptools                                     # cpp tools
-code --install-extension ms-vscode.cpptools-extension-pack                      # cpp extension
-code --install-extension ms-vscode.cpptools-themes                              # cpp theme
-code --install-extension ms-vscode.remote-explorer                              # remote explorer
-code --install-extension ms-vsliveshare.vsliveshare                             # video call/code sharing for code review
-code --install-extension redhat.vscode-yaml                                     # yaml support
-code --install-extension ritwickdey.LiveServer                                  # live web server
-code --install-extension twxs.cmake                                             # cmake support
+flatpak run com.visualstudio.code  --install-extension atlassian.atlascode                                    # jira issue
+flatpak run com.visualstudio.code  --install-extension GitHub.vscode-pull-request-github                      # github issue
+flatpak run com.visualstudio.code  --install-extension GitHub.remotehub                                       # github explorer
+flatpak run com.visualstudio.code  --install-extension GitHub.copilot                                         # github copilot ai
+flatpak run com.visualstudio.code --install-extension GitHub.copilot-chat                                     # github copilot ai chat
+flatpak run com.visualstudio.code  --install-extension Gruntfuggly.todo-tree                                  # todo from code
+flatpak run com.visualstudio.code  --install-extension ms-azuretools.vscode-docker                            # container
+flatpak run com.visualstudio.code  --install-extension ms-vscode-remote.remote-containers                     # remote contaienr
+flatpak run com.visualstudio.code  --install-extension ms-vscode-remote.remote-ssh                            # remote ssh
+flatpak run com.visualstudio.code  --install-extension ms-vscode-remote.remote-ssh-edit                       # edit remote ssh
+flatpak run com.visualstudio.code  --install-extension ms-vscode.cmake-tools                                  # smake tools support
+flatpak run com.visualstudio.code  --install-extension ms-vscode.cpptools                                     # cpp tools
+flatpak run com.visualstudio.code  --install-extension ms-vscode.cpptools-extension-pack                      # cpp extension
+flatpak run com.visualstudio.code  --install-extension ms-vscode.cpptools-themes                              # cpp theme
+flatpak run com.visualstudio.code  --install-extension ms-vscode.remote-explorer                              # remote explorer
+flatpak run com.visualstudio.code  --install-extension ms-vsliveshare.vsliveshare                             # video call/code sharing for code review
+flatpak run com.visualstudio.code  --install-extension redhat.vscode-yaml                                     # yaml support
+flatpak run com.visualstudio.code  --install-extension ritwickdey.LiveServer                                  # live web server
+flatpak run com.visualstudio.code  --install-extension twxs.cmake                                             # cmake support
+flatpak run com.visualstudio.code --install-extension shd101wyy.markdown-preview-enhanced                     # markdown preview
+flatpak run com.visualstudio.code --install-extension ya-technologies.vslabx-extension                        # mplabx support
